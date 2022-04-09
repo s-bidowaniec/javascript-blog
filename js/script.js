27a4e14a44e966abbd1645adb48c772d7ecf181c
Stanisław Bidowaniec <20791742+s-bidowaniec@users.noreply.github.com>
2022-04-09 15:32:23 +0200

    Change titleClickHandler, add 'active' class to link

--- js/script.js
+++ js/script.js
@@ -7,15 +7,18 @@
     });*/
 
     const titleClickHandler = function(event) {
-        console.log(event);
-        /* remove class 'active' from all article links  */
+        const clickedElement = this;
+        console.log('Link was clicked!');
+        /* [DONE] remove class 'active' from all article links  */
         const activeLinks = document.querySelectorAll('.titles a.active');
         for(let activeLink of activeLinks){
             activeLink.classList.remove('active');
         }
-        /* add class 'active' to the clicked link */
+        /* [IN PROGRESS] add class 'active' to the clicked link */
+        console.log('clickedElement:', clickedElement);
+        clickedElement.classList.add('active');
 
-        /* remove class 'active' from all articles */
+        /* [DONE] remove class 'active' from all articles */
         const activeArticles = document.querySelectorAll('.post.active');
         for (let activeArticle of activeArticles){
             activeArticle.classList.remove('active')
