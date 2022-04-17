93f677d5def8aaac7f29bfb47f1317431ab3f755
Stanisław Bidowaniec <20791742+s-bidowaniec@users.noreply.github.com>
2022-04-17 16:15:39 +0200

    Fix title link remove active class on click

--- js/script.js
+++ js/script.js
@@ -36,7 +36,7 @@
     const clickedElement = this;
     console.log('Link was clicked!');
     /* [DONE] remove class 'active' from all article links  */
-    const activeLinks = document.querySelectorAll(select.listOf.titles+'a.active');
+    const activeLinks = document.querySelectorAll(select.listOf.titles+' a.active');
     for(let activeLink of activeLinks){
       activeLink.classList.remove('active');
     }
